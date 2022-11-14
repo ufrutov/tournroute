89169e51698a16ee386ba541af7d085c981596ac
ufrutov <mailluifruti@mail.ru>
2022-11-14 20:57:06 +0200

    Update City page - add description lead

--- pages/city/index.tsx
+++ pages/city/index.tsx
@@ -4,11 +4,12 @@ import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 import DataManager from "@/util/DataManager";
 import { City, Place } from "@/util/Interface";
 import { Banner } from "@/styled/Banner";
-import { Container } from "@/styled/PageContent";
+import { Container, PlaceDescription } from "@/styled/PageContent";
 import { Empty } from "@/styled/Empty";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
@@ -69,31 +70,38 @@ const City: NextPage = () => {
 				<Container className="container py-5">
 					<div className="row g-4">
 						{ready ? (
-							places.length > 0 ? (
-								places.map((place: Place) => (
-									<Link href={`place?id=${place.sys.id}`} key={place.sys.id as Key}>
-										<SimpleCard className="col-lg-3 col-md-4 col-6">
-											<div>
-												<SimpleCardText className="p-4">
-													<h2>{place.name}</h2>
-													<p>
-														<strong>{place.city.name}</strong>
-													</p>
-												</SimpleCardText>
-												<Image
-													src={place.galleryCollection.items[0].url}
-													loader={({ src }) => src}
-													layout="fill"
-												/>
-											</div>
-										</SimpleCard>
-									</Link>
-								))
-							) : (
-								<Empty>
-									<h2 className="text-center">No places here...</h2>
-								</Empty>
-							)
+							<>
+								<div className="col-lg-6 col-md-12 col-12">
+									<PlaceDescription className="p-4">
+										<p className="lead">{documentToReactComponents(city!.description.json)}</p>
+									</PlaceDescription>
+								</div>
+								{places.length > 0 ? (
+									places.map((place: Place) => (
+										<Link href={`place?id=${place.sys.id}`} key={place.sys.id as Key}>
+											<SimpleCard className="col-lg-3 col-md-4 col-6">
+												<div>
+													<SimpleCardText className="p-4">
+														<h2>{place.name}</h2>
+														<p>
+															<strong>{place.city.name}</strong>
+														</p>
+													</SimpleCardText>
+													<Image
+														src={place.galleryCollection.items[0].url}
+														loader={({ src }) => src}
+														layout="fill"
+													/>
+												</div>
+											</SimpleCard>
+										</Link>
+									))
+								) : (
+									<Empty>
+										<h2 className="text-center">No places here...</h2>
+									</Empty>
+								)}
+							</>
 						) : (
 							empty.map((e) => (
 								<SimpleCard className="col-lg-3 col-md-4 col-6" key={e} empty={true}>
